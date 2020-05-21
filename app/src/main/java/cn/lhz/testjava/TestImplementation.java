43ab10e05f554fcaf9faa7684c7c49ba386a8ea0
LiuHanzhi <hanzhi@staff.sina.com>
2020-05-21 19:32:48 +0800

    add app中通过implementation依赖远程lib。

---
+++ app/src/main/java/cn/lhz/testjava/TestImplementation.java
@@ -0,0 +1,11 @@
+package cn.lhz.testjava;
+
+import cn.lhz.lib_java.thread.TestProcessor;
+
+public class TestImplementation {
+
+    public static void ttt() {
+        TestProcessor testProcessor = new TestProcessor();
+        testProcessor.run();
+    }
+}
